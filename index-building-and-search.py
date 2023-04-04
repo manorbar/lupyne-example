498c51bd9a3337df043914bdf389e50d4ffcca74
Manor Bar <manorbar12@gmail.com>
2023-04-04 08:32:29 +0000

    testing of polygon creation

--- index-building-and-search.py
+++ index-building-and-search.py
@@ -1,51 +1,56 @@
 import lucene
-import json
-from java.io import File
-from org.apache.lucene import analysis, document, index, queryparser, search, store, geo
-from lupyne.lupyne import engine
-from shapely import Polygon
-
-assert lucene.initVM()
-
 from datetime import date
+import json
+
+import java
+from java.io import File
+# from org.apache.lucene.geo import Polygon, Point
+from org.apache.lucene import analysis, document, index, queryparser, search, store, geo
+# from jarray import array
+from lupyne import engine
+from shapely import Polygon
+from org.apache.lucene.document import LatLonPoint
+
+from org.apache.lucene.spatial3d.geom import GeoPolygonFactory, GeoPoint, GeoPolygon, PlanetModel, XYZBounds
+from org.apache.lucene.geo import XYPolygon
+
+
+lucene.initVM()
+
+
 # Using readlines()
-file1 = open('lupyne-example/sep.txt', 'r')
+file1 = open('/home/sep.txt', 'r')
 Lines = file1.readlines()
   
 geometries = []
 features = []
-for line in Lines[1:5]:
-    feature = json.loads(line.strip())
-    geometries.append(feature['geometry'])
-    features.append(feature)
-    
+try:
+    # POLYGON((30 10, 40 40, 20 40, 10 20, 30 10))
+    for line in Lines:
+        feature = json.loads(line.strip())
+        geometries.append(feature['geometry'])
+        poly = Polygon([[p[0], p[1]] for p in feature['geometry']['coordinates'][0]])
+        poly2 = geo.Polygon([p[0] for p in feature['geometry']['coordinates'][0]], [p[1] for p in feature['geometry']['coordinates'][0]])
+        # Convert the polygon to a GeoShape
+        # geo_points = [GeoPoint(PlanetModel.WGS84, point[0], point[1]) for point in poly.exterior.coords[:-1]]
+        geo_points = [GeoPoint(PlanetModel.WGS84, p[0], p[1], p[2]) for p in feature['geometry']['coordinates'][0]]
+        geo_polygon = GeoPolygonFactory.makeGeoPolygon(PlanetModel.WGS84, geo_points)
+        geo_points = [GeoPoint(p[0], p[1], p[2]) for p in feature['geometry']['coordinates'][0]]
+        # geo_points.append(GeoPoint(feature['geometry']['coordinates'][0][-1][0], feature['geometry']['coordinates'][0][-1][1], feature['geometry']['coordinates'][0][-1][2]))
+        # poly = XYPolygon([30, 40, 20, 10, 30], [10, 40, 40, 20, 10])
+        
+        polygon = GeoPolygonFactory.makeGeoPolygon(PlanetModel.WGS84, geo_points)
+        # polygon = GeoPolygonFactory.makeGeoPolygon(feature['geometry']['coordinates'][0])
+        # poly = XYPolygon([p[0] for p in feature['geometry']['coordinates'][0]], [p[1] for p in feature['geometry']['coordinates'][0]])
+        # Create a list of GeoPoints from your list of points
+        point_list = [(x[0], x[1], x[2]) for x in feature['geometry']['coordinates'][0]]
+        points = [GeoPoint.fromRadians(p[0], p[1]) for p in point_list]
 
-docs = [
-    {
-        'city': 'San Francisco',
-        'state': 'CA',
-        'population': 808976,
-        'location': geometries[0]['coordinates'][0],
-        'feature': features[0]
-    },
-    {
-        'city': 'Los Angeles',
-        'state': 'CA',
-        # 'incorporated': '1850-04-04',
-        'population': 3849378,
-        'location': geometries[1]['coordinates'][0],
-        'feature': features[1]
-    },
-    {
-        'city': 'Portland',
-        'state': 'OR',
-        # 'incorporated': '1851-02-08',
-        'population': 575930,
-        'location': geometries[2]['coordinates'][0],
-        'feature': features[2]
-    },
-]
-
+        # Create a GeoPolygon from the list of GeoPoints
+        polygon = GeoPolygonFactory.makeGeoPolygon(PlanetModel.WGS84, points)
+        features.append(feature)
+except BaseException as e:
+    print(e)
 indexer = engine.Indexer('tempIndex')
 indexer.set('city', stored=True)
 indexer.set('state', stored=True)
